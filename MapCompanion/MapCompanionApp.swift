0b9b9c8dc0ab2a341505b0e3591d333e6241f10b
Miguel Ángel Cofrancesco <mikicof@mandamelon.com>
2026-03-10 18:30:21 +0100

    Initial Commit

---
+++ MapCompanion/MapCompanionApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapCompanionApp.swift
+//  MapCompanion
+//
+//  Created by Miguel Ángel Cofrancesco on 10/3/26.
+//
+
+import SwiftUI
+
+@main
+struct MapCompanionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
